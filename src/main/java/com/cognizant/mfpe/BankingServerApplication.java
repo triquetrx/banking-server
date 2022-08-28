d09f4334e3cde80088adb60bc80d0ccaccb7bded
Khan <2146122@cognizant.com>
2022-08-28 19:29:18 +0530

    eureka-banking-server

---
+++ src/main/java/com/cognizant/mfpe/BankingServerApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.mfpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BankingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/mfpe/BankingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.mfpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
